59b7aa9aee4c5845cb7736cf4b0df6b33a3e552e
Rajeev Singh <rajeevsinghece49@gmail.com>
2025-10-12 22:15:23 +0530

    feat(disable-login-methods): explored disabling formLogin() and httpBasic() → formLogin(): handled by UsernamePasswordAuthenticationFilter (HTML form credentials) → httpBasic(): handled by BasicAuthenticationFilter (Base64 in Authorization header) → disabling both removes all login mechanisms, leading to 403 responses

--- src/main/java/com/eazybytes/springsecsection1/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/springsecsection1/config/ProjectSecurityConfig.java
@@ -1,9 +1,8 @@
 package com.eazybytes.springsecsection1.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
