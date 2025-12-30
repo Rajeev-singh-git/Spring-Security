c02d6fa03342663e6caf47595794e7a719b74570
Rajeev  Singh <rajeevsinghece49@gmail.com>
2025-12-30 16:33:14 +0530

    Section 4 setup : Defining and managing user using db

---
+++ 4. Defining & Managing users using a db/src/main/java/easybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,50 @@
+package easybytes.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+                .requestMatchers("/notice","/contact","/error").permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user =  User.withUsername("user").password("{bcrypt}User@12345").authorities("read").build();
+        UserDetails admin =  User.withUsername("admin").password("{bcrypt}$2a$12$Md/zV5nblMMi4W4.rytxOOJ82c9B4mldOcQG5MBG1ZjsI2g/ycfia").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker(){
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+
+
+}
