cf8f7200212fb8669bdd6758cb4f4def1dfe3b4e
Rajeev  Singh <rajeevsinghece49@gmail.com>
2026-01-04 15:50:35 +0530

    Custom Authentication Handler Added

--- 7. Sprinng Security Customizations/eazyschool-start/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ 7. Sprinng Security Customizations/eazyschool-start/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -18,7 +18,11 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
+@RequiredArgsConstructor
 public class ProjectSecurityConfig {
+
+    private final CustomAuthenticationSuccessHandler authenticationSuccessHandler;
+    private final CustomAuthenticationFailureHandler authenticationFailureHandler;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -28,7 +32,8 @@ public class ProjectSecurityConfig {
                         .requestMatchers("/", "/home", "/holidays/**", "/contact", "/saveMsg",
                                 "/courses", "/about", "/assets/**","/login/**").permitAll())
                 .formLogin(flc->flc.loginPage("/login").usernameParameter("userid").passwordParameter("secretPwd")
-                        .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true"))
+                        .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true")
+                        .successHandler(authenticationSuccessHandler).failureHandler(authenticationFailureHandler))
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
