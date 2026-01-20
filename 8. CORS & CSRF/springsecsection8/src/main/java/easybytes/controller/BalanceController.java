6a5dbad9aa60ce08af4ec9ee5e765ebb8962f770
Rajeev  Singh <rajeevsinghece49@gmail.com>
2026-01-20 21:14:57 +0530

    Backend code updated

--- 8. CORS & CSRF/springsecsection8/src/main/java/easybytes/controller/BalanceController.java
+++ 8. CORS & CSRF/springsecsection8/src/main/java/easybytes/controller/BalanceController.java
@@ -1,13 +1,28 @@
 package easybytes.controller;
 
+import easybytes.model.AccountTransactions;
+import easybytes.repository.AccountTransactionsRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class BalanceController {
 
+    private final AccountTransactionsRepository accountTransactionsRepository;
+
     @GetMapping("/myBalance")
-    public String getAccountDetail(){
-        return "Here are the balance  details from the DB";
+    public List<AccountTransactions> getBalanceDetails(@RequestParam long id) {
+        List<AccountTransactions> accountTransactions = accountTransactionsRepository.
+                findByCustomerIdOrderByTransactionDtDesc(id);
+        if (accountTransactions != null) {
+            return accountTransactions;
+        } else {
+            return null;
+        }
     }
 }
--- 8. CORS & CSRF/springsecsection8/src/main/java/easybytes/controller/NoticesController.java
+++ 8. CORS & CSRF/springsecsection8/src/main/java/easybytes/controller/NoticesController.java
@@ -1,13 +1,31 @@
 package easybytes.controller;
 
+import easybytes.model.Notice;
+import easybytes.repository.NoticeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 @RestController
+@RequiredArgsConstructor
 public class NoticesController {
 
-    @GetMapping("/notice")
-    public String getNotices(){
-        return "Here are the Notices details from the DB";
+    private final NoticeRepository noticeRepository;
+
+    @GetMapping("/notices")
+    public ResponseEntity<List<Notice>> getNotices() {
+        List<Notice> notices = noticeRepository.findAllActiveNotices();
+        if (notices != null) {
+            return ResponseEntity.ok()
+                    .cacheControl(CacheControl.maxAge(60, TimeUnit.SECONDS))
+                    .body(notices);
+        } else {
+            return null;
+        }
     }
 }
