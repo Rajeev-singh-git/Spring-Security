54db38d2a97b7beb9c51ffffcde5031964c11551
Rajeev  Singh <rajeevsinghece49@gmail.com>
2026-01-25 21:47:12 +0530

    Script added

--- 9. Authorization Implementation/src/main/resources/scripts/scripts2.sql
+++ 9. Authorization Implementation/src/main/resources/scripts/scripts2.sql
@@ -163,3 +163,41 @@ CREATE TABLE `contact_messages` (
   `create_dt` date DEFAULT NULL,
   PRIMARY KEY (`contact_id`)
 );
+
+-- Section 9 script
+
+CREATE TABLE `authorities` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int NOT NULL,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`)
+);
+
+-- Insert
+
+INSERT INTO `authorities` (`customer_id`, `name`)
+ VALUES (1, 'VIEWACCOUNT');
+
+INSERT INTO `authorities` (`customer_id`, `name`)
+ VALUES (1, 'VIEWCARDS');
+
+INSERT INTO `authorities` (`customer_id`, `name`)
+  VALUES (1, 'VIEWLOANS');
+
+INSERT INTO `authorities` (`customer_id`, `name`)
+   VALUES (1, 'VIEWBALANCE');
+
+
+--   Delete
+
+DELETE FROM `authorities`;
+
+-- Insert
+
+INSERT INTO `authorities` (`customer_id`, `name`)
+  VALUES (1, 'ROLE_USER');
+
+INSERT INTO `authorities` (`customer_id`, `name`)
+  VALUES (1, 'ROLE_ADMIN');
