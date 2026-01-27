3c97bc704b6566ce563d0e77b03150dc79851cfc
Rajeev  Singh <rajeevsinghece49@gmail.com>
2026-01-27 19:12:46 +0530

    S11 : Implemented Logic to Generate JWT tokens

---
+++ 11. JSON Web Token/src/main/java/easybytes/constants/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package easybytes.constants;
+
+public final class ApplicationConstants {
+
+    public static final String JWT_SECRET_KEY = "JWT_SECRET";
+    public static final String JWT_SECRET_DEFAULT_VALUE = "jxgEQioYD8228917bs83";
+    public static final String JWT_HEADER = "Authorization";
+}
